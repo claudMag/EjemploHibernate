4b91aa34a4aedc5b942b01de700f0e0d0d8fe52c
CLAUDIAMAGGIO <claudia.maggio1@gmail.com>
2022-11-22 11:59:03 +0100

    fatto

--- src/LogicaNegocio.java
+++ src/LogicaNegocio.java
@@ -1,5 +1,8 @@
 import mapeo.Cliente;
 import org.hibernate.Session;
+
+import java.util.List;
+
 public class LogicaNegocio {
 
     private HibernateUtil utilitario;
@@ -11,9 +14,19 @@ public class LogicaNegocio {
     }
     public void ejecutarAccionesPrototipo(){
         //ejemplo de inserción de un registro
-        session.beginTransaction();
+       /* session.beginTransaction();
         Cliente cliente = new Cliente(1, "Papelaria Pepito", "Valencia", 500.5f);
         session.save(cliente);
+        session.getTransaction().commit();*/
+
+        //ejemplo de consulta
+        session.beginTransaction();
+        //from ---> nombre de la clase, da igual el nombre de la tabla
+        List<Cliente> clienteList = session.createQuery("from Cliente").list();
+        for (Cliente c: clienteList) {
+            System.out.println(c.toString());
+        }
         session.getTransaction().commit();
+
     }
 }
